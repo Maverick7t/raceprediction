82921ac3ffd6a701fd8e8332a0ae1c9c10d8dc90
folder structure added <moinaktarshaikh651@gmail.com>
2026-05-24 13:03:54 +0530

    resuls mapping for db

---
+++ backend/app/db/models/results.py
@@ -0,0 +1,118 @@
+from datetime import datetime
+from sqlalchemy import (
+    Binginteger,
+    CheckConstraint,
+    Float,
+    Index,
+    SmallInteger,
+    Text,
+    TIMESTAMP,
+    UniqueConstraint,
+    func,
+)
+
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+
+)
+
+from app.db.base import Base
+
+class ResultRaw(Base):
+    __tablename__ = "results_raw"
+
+    id: Mapped[int] = mapped_column(
+        Binginteger, 
+        primary_key=True,
+    )
+
+    race_key: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    driver_code: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    driver_id: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    driver_name: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    team: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    team_id: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    grid_position: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False,
+    )
+
+    finish_position: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False,
+    )
+
+    points: Mapped[float] = mapped_column(
+        Float,
+        nullable=False,
+    )
+
+    status: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    year: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False,
+    )
+
+    round: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False,
+    )   
+
+    race_name: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    circuit_id: Mapped[str] = mapped_column(
+        Text,
+         nullable=False,
+    )
+
+    source: Mapped[str] = mapped_column(
+        Text,
+         nullable=False,
+    )
+
+    ingested_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    )
+
+    __table_args__ = (
+        UniqueConstraint(
+            "race_key",
+            "driver_code",
+            name="race_driver_unique",
+        ),
+
+        
